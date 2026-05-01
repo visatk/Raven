aec7cce145bb70c003c9fc71e656add75d99d1a7
visatk <visatk29@gmail.com>
2026-05-02 04:50:36 +0600

    Add RATE_CACHE to Env interface

--- src/types.ts
+++ src/types.ts
@@ -4,13 +4,13 @@ import * as schema from './db/schema';
 
 export interface Env {
   DB: D1Database;
+  RATE_CACHE: KVNamespace;
   TELEGRAM_BOT_TOKEN: string;
   ADMIN_TELEGRAM_ID: string;
   APIRONE_ACCOUNT: string;
   PUBLIC_WEBHOOK_URL: string;
 }
 
-// Extend the standard grammY context with our database and environment
 export interface BotContext extends Context {
   db: DrizzleD1Database<typeof schema>;
   env: Env;
